2f80c15781fe6700e575a5e9d928a43b2da97739
Dengfeng Liu <liudf0716@qq.com>
2017-03-13 17:11:43 +0800

    Create arp_get.c

---
+++ arp_get.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <net/if_arp.h>
+
+
+int arp_get_mac(const char *i_ip, char *o_mac) {
+}
+
+int arp_get_ip(const char *i_mac, char *o_ip) {
+}
