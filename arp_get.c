fbb7780dbf088d4d467419ee7be41c43b95b1e20
Dengfeng Liu <liudf0716@qq.com>
2017-03-13 19:29:04 +0800

    Update arp_get.c

--- arp_get.c
+++ arp_get.c
@@ -10,8 +10,39 @@
 #include <net/if_arp.h>
 
 
-int arp_get_mac(const char *i_ip, char *o_mac) {
+int arp_get_mac(const char *dev_name, const char *i_ip, char *o_mac) {
+	int s;
+    struct arpreq arpreq;
+    struct sockaddr_in *sin;
+	
+	if (!dev_name || !i_ip || !o_mac)
+		return 0;
+	
+	s = socket(AF_INET, SOCK_DGRAM, 0);
+	if (s <= 0) {
+		return 0;
+	}
+	
+	memset(&arpreq, 0, sizeof(arpreq));
+
+    sin = (struct sockaddr_in *) &arpreq.arp_pa;
+    sin->sin_family = AF_INET;
+    sin->sin_addr.s_addr = inet_addr(i_ip);
+
+	strcpy(arpreq.arp_dev, dev_name);
+	if (ioctl(s, SIOCGARP, &arpreq) < 0) {
+		return 0;
+	}
+	
+	if (arpreq.arp_flags & ATF_COM) {
+        unsigned char *eap = (unsigned char *) &arpreq.arp_ha.sa_data[0];
+        sscanf(o_mac, "%02X:%02X:%02X:%02X:%02X:%02X",
+                eap[0], eap[1], eap[2], eap[3], eap[4], eap[5]);
+        return 1;
+    } 
+	
+	return 0;
 }
 
-int arp_get_ip(const char *i_mac, char *o_ip) {
+int arp_get_ip(const char *dev_name, const char *i_mac, char *o_ip) {
 }
