42d87ef4af535b2372133d3b25a100fcd9295a6a
Dengfeng Liu <liudf0716@qq.com>
2017-03-13 17:12:12 +0800

    Update arp_get.h

--- arp_get.h
+++ arp_get.h
@@ -1,5 +1,15 @@
 #ifdef	_ARP_GET_
 #define	_ARP_GET_
+
+/*  arp_flags and at_flags field values */
+#define ATF_INUSE   0x01    /* entry in use */
+#define ATF_COM     0x02    /* completed entry (enaddr valid) */
+#define ATF_PERM    0x04    /* permanent entry */
+#define ATF_PUBL    0x08    /* publish entry (respond for other host) */
+#define ATF_USETRAILERS 0x10    /* has requested trailers */
+#define ATF_PROXY   0x20    /* Do PROXY arp */
+
+
 int arp_get_mac(const char *i_ip, char *o_mac);
 int arp_get_ip(const char *i_mac, char *o_ip);
 #endif
