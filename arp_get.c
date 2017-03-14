a035d05eb81f52c4ca9c0335455f18054a2af430
Dengfeng Liu <liudf0716@qq.com>
2017-03-14 11:35:00 +0800

    Update arp_get.c

--- arp_get.c
+++ arp_get.c
@@ -8,10 +8,63 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <net/if_arp.h>
+/* libevent */
+#include <event-config.h>
+#include <event.h>
+/* openssl */
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
 #include "arp_get.h"
+#include "http_client.h"
 
-int mac_2_vendor(const char *i_mac, char *o_vender) {
+static int get_mac_vendor_url(const char *mac, char *o_url, int length) {
+	if (!mac || !o_url)
+		return 0;
+	
+	int nret = snprintf(o_url, length, "%s/%s", MAC_VENDOR_LOOKUP_URL, mac);
+	return nret<0?0:1;
+}
+
+static void mac_2_vendor(struct http_client *api, struct http_request *req, struct http_response *response, void *baton) {
+	struct s_vendor *vendor = (struct s_vendor *)baton;
+	printf("%.*s", response->body_len, response->body);
+}
+
+#define LOOKUP_URL_LENGTH	128
+#define VENDOR_LENGTH		128
+
+int get_mac_vendor(const char *mac) {
+	char lookup_url[LOOKUP_URL_LENGTH] = {0};
+	if (!get_mac_vendor_url(mac, lookup_url, LOOKUP_URL_LENGHT))
+		return 0;
+	
+	struct s_vendor *vendor = malloc(sizeof(struct s_vendor));
+	memset(vendor, 0, sizeof(struct s_vendor));
+	make_http_get_request(lookup_url, mac_2_vendor, vendor);
+}
+
+void make_http_get_request(const char *url,  cb_http_response callback, void *baton) {
+	
+	
+	struct evhttp_uri *http_uri = evhttp_uri_parse(url);
+	const char *host = evhttp_uri_get_host(http_uri);
+	const int  port = evhttp_uri_get_port(http_uri);
+	
+	SSL_load_error_strings();
+	ERR_load_BIO_strings();
+	OpenSSL_add_all_algorithms();
+
+	event_init();
+	
+	struct http_client   *client = http_client_new(host, port, NULL);
+  	struct http_request  *req = http_client_request(client, HTTP_METHOD_GET, strlen(url), url);
+
+  	http_client_request_dispatch(req, callback, baton);
+	
+	event_loop(0);
+	
 	return 0;
 }
 
